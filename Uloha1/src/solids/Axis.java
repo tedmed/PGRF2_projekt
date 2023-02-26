ab41c71523bd8e602edd87f97cecf0b8d18bb74d
Ted <tadeasj@email.cz>
2023-02-26 20:09:26 +0100

    axis arrows

--- Uloha1/src/solids/Axis.java
+++ Uloha1/src/solids/Axis.java
@@ -8,8 +8,20 @@ public class Axis extends Solid{
     public Axis(double x, double y, double z) {
         vertexBuffer.add(new Point3D(0,0,0));
         vertexBuffer.add(new Point3D(x,y,z));
+        if(x == 1) {
+            vertexBuffer.add(new Point3D(x*0.85, y+0.15*x, 0));
+            vertexBuffer.add(new Point3D(x*0.85, 0, z+0.15*x));
+        }
+        if(y == 1) {
+            vertexBuffer.add(new Point3D(x+0.15*y, y*0.85, 0));
+            vertexBuffer.add(new Point3D(0, y*0.85, z+0.15*y));
+        }
+        if(z == 1) {
+            vertexBuffer.add(new Point3D(0, y+0.15*z, z*0.85));
+            vertexBuffer.add(new Point3D(x+0.15*z, 0, z*0.85));
+        }
         indexBuffer = new ArrayList<>();
 
-        addIndices(0,1);
+        addIndices(0,1, 1,2, 1,3);
     }
 }
